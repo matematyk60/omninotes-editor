420426f991f614c2f258f811a21873f15a1a7632
Grzegorz Nieużyła <grzegorznieuzyla@gmail.com>
2020-04-02 17:28:23 +0200

    CLI improvements

--- omninotes/cli.py
+++ omninotes/cli.py
@@ -1,27 +1,29 @@
 import argparse
+import sys
 from omninotes.importer import Importer
+
 
 class CLI:
     def __init__(self):
-
         self.parser = argparse.ArgumentParser(description="OmniNotes editor")
-        self.parser.add_argument("-i", "--import", dest="import_", action='store_true')
-        self.parser.add_argument("-e", "--export", dest="export", action='store_true')
-        self.parser.add_argument("-p", "--path", dest="path")
-        self.parser.add_argument("-d", "--destination", dest="dest")
-
+        self.parser.add_argument(
+            "-i", "--import", dest="import_", action='store_true')
+        self.parser.add_argument(
+            "-e", "--export", dest="export", action='store_true')
+        self.parser.add_argument(
+            "-d", "--destination", dest="destination", action="store")
+        self.parser.add_argument("backup_paths", nargs='+', action="store")
 
     def run(self):
         options = self.parser.parse_args()
-        if not options.path or (options.import_ and options.export):
+        if bool(options.import_) == bool(options.export):
             self.parser.print_usage()
         elif options.import_:
-            dest = options.dest if options.dest else "./OmniNotesEditor/"
-            Importer(options.path).importNotes(dest)
-        elif options.export:
-            raise NotImplementedError()
+            dest = options.destination if options.destination else "./OmniNotesEditor/"
+            for backup_path in options.backup_paths:
+                try:
+                    Importer(backup_path).importNotes(dest)
+                except Exception as e:
+                    print(f"Error while importing backup '{backup_path}': {e}")
         else:
-            self.parser.print_usage()
-
-
-    
+            raise NotImplementedError()
